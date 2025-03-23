e2dd9e63a3bac36cf340193fe91f45b1ed6150a8
Rikki <rikki@member.fsf.org>
2025-03-24 05:34:12 +0800

    fix: nixos can not install pnpm pkgs

--- flake.nix
+++ flake.nix
@@ -14,10 +14,33 @@
 
       in {
         devShell = pkgs.mkShell {
-          # add things you want in your shell here
           buildInputs = with pkgs; [
             nodejs
+            pnpm
+            git
+            cacert
+            gnupg
           ];
+          
+          sandboxPaths = [ "/etc/ssl" "/tmp" ];
+          
+          shellHook = ''
+            export PNPM_HOME="$(pwd)/.pnpm-store"
+            export PNPM_STORE_DIR="$PNPM_HOME"
+            mkdir -p $PNPM_HOME
+            
+            export PNPM_VERIFY_STORE_INTEGRITY=false
+            export PNPM_STRICT_SSL=false
+            export PNPM_IGNORE_WORKSPACE=true
+            
+            export COREPACK_ENABLE_STRICT=0
+            export COREPACK_DOWNLOAD_MIRROR=https://npmmirror.com/mirrors/corepack/
+            
+            export SSL_CERT_FILE="${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
+            export NODE_EXTRA_CA_CERTS="${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
+            
+            export npm_config_arch="x64"
+          '';
         };
       }
     );
